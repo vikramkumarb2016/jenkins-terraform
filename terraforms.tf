89381d0d77544bbeba2abd139e68c6c3a1d3e1e8
VikramKumar <vikramkumarb2016@gmail.com>
2023-08-22 19:57:32 +0530

    Create terraforms.tf

---
+++ terraforms.tf
@@ -0,0 +1,10 @@
+resource "local_file" "My-family"{
+    filename = var.filename
+       content = var.content
+
+       }
+
+resource "random_pet" "family" {
+  prefix = "MOM"
+  separator = "."
+  length = "1"
