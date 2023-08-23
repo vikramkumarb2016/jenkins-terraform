72c8f3033956f17bf1a00960317758e6b6174ff9
VikramKumar <vikramkumarb2016@gmail.com>
2023-08-23 14:58:03 +0530

    Update terraforms.tf

--- terraforms.tf
+++ terraforms.tf
@@ -1,10 +1,9 @@
 resource "local_file" "My-family"{
     filename = "family.txt"
-       content = " I LOve Family"
-
+       content = "I Love Family"
        }
-
 resource "random_pet" "family" {
   prefix = "MOM"
   separator = "."
   length = "1"
+}
